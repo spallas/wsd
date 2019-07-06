5672453787175f3f827a2ee9d0c179aa95ecff1e
spallas <davide.spallaccini@gmail.com>
2019-07-06 07:22:46 +0200

    Bug fix in processing. Add lemma-pos loader. Move test subclasses in testing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,11 +3,10 @@ Load data from SemCor files and SemEval/SensEval files.
 """
 
 import xml.etree.ElementTree as Et
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import List, Dict
 
 from allennlp.modules.elmo import batch_to_ids
-from torch import Tensor
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
@@ -18,7 +17,8 @@ class SemCorDataset(Dataset):
 
     def __init__(self,
                  data_path='res/wsd-train/semcor_data.xml',
-                 tags_path='res/wsd-train/semcor_tags.txt'):
+                 tags_path='res/wsd-train/semcor_tags.txt',
+                 sense2id: Dict[str, int] = None):
         """
         Load from XML and txt files sentences and tags.
         :param data_path: path to XML SemCor-like file.
@@ -31,13 +31,19 @@ class SemCorDataset(Dataset):
         for sl in instance2senses.values():
             senses_list += sl
         c = Counter(senses_list)
-        # Build sense to id index
-        sense2id: Dict[str, int] = {w: i for i, w in enumerate(list(c.keys()), 1)}
+        # Build sense to id index if training dataset. Take from args if test dataset.
+        if not sense2id:
+            sense2id: Dict[str, int] = defaultdict(lambda: -1)
+            # WARNING: dict value for senses not seen at training time is -1
+            for i, w in enumerate(list(c.keys()), start=1):
+                sense2id[w] = i
+        self.sense2id = sense2id
         # 0 for monosemic words, no sense associated
         self.senses_count = {sense2id[k]: v for k, v in c.items()}
+        if sense2id:  # i.e. loading training set
+            self.senses_count[-1] = 0
         instance2ids: Dict[str, List[int]] = {k: list(map(lambda x: sense2id[x], v)) for k, v in instance2senses.items()}
 
-        self.elmo_documents: List[Tensor] = []
         self.docs: List[List[str]] = []
         self.senses: List[List[List[int]]] = []
         self.first_senses: List[List[int]] = []
@@ -56,11 +62,14 @@ class SemCorDataset(Dataset):
                     senses.append(word_senses)
                     if lemma not in self.vocab:
                         self.vocab[lemma] = len(self.vocab)
-            self.elmo_documents.append(batch_to_ids([lemmas])[0])
             self.docs.append(lemmas)
             self.pos_tags.append(pos_tags)
             self.senses.append(senses)
             self.first_senses.append([i[0] for i in senses])
+        # sort documents by length to minimize padding.
+        z = zip(self.docs, self.pos_tags, self.senses, self.first_senses)
+        sorted_z = sorted(z, key=lambda x: len(x[0]))
+        self.docs, self.pos_tags, self.senses, self.first_senses = map(lambda x: list(x), zip(*sorted_z))
 
     def __len__(self):
         return len(self.docs)
@@ -77,20 +86,17 @@ class SemCorDataLoader:
                  win_size: int,
                  shuffle: bool = False,
                  overlap_size: int = 0,
-                 return_all_senses: bool = False,
-                 return_pos_tags: bool = False):
+                 return_all_senses: bool = False):
         self.dataset = dataset
         self.batch_size = batch_size
         self.win_size = win_size
         self.do_shuffle = shuffle
         self.overlap_size = overlap_size
         self.do_return_all_senses = return_all_senses
-        self.do_return_pos = return_pos_tags
 
     def __iter__(self):
         self.last_doc = 0
         self.last_offset = 0
-        self.stop_iter = False
         return self
 
     def __next__(self):
@@ -105,22 +111,22 @@ class SemCorDataLoader:
             y: Tensor - shape = (batch_size x win_size)
                       - y[i][j] = sense index in vocab
         """
-        if self.stop_iter:
-            raise StopIteration
-        b_x = []
-        b_y = []
-        b_l = []
+        stop_iter = False
+        b_x, b_l, b_y = [], [], []
         lengths = [len(d) for d in self.dataset.docs[self.last_doc: self.last_doc + self.batch_size]]
         end_of_docs = max(lengths) <= self.last_offset + self.win_size
         for i in range(self.batch_size):
-            if self.last_doc + i >= len(self.dataset.docs) - 1:
-                self.stop_iter = True
+            if self.last_doc + i >= len(self.dataset.docs):
+                stop_iter = True
                 break
-            text_span = self.dataset.docs[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
+            n = self.last_doc + i
+            m = slice(self.last_offset, self.last_offset + self.win_size)
+            text_span = self.dataset.docs[n][m]
+            labels = self.dataset.first_senses[n][m]
             text_span_ids = list(map(lambda x: self.dataset.vocab[x], text_span))
-            labels = self.dataset.first_senses[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
             length = len(text_span)
             # Padding
+            text_span += ['PAD'] * (self.win_size - len(text_span))
             text_span_ids += [self.dataset.vocab['PAD']] * (self.win_size - len(text_span_ids))
             labels += [self.dataset.vocab['PAD']] * (self.win_size - len(labels))
             b_x.append(text_span)
@@ -131,32 +137,37 @@ class SemCorDataLoader:
         if end_of_docs:
             self.last_doc += self.batch_size
             self.last_offset = 0
+            if stop_iter or self.last_doc >= len(self.dataset.docs):
+                raise StopIteration
+
         return b_x, b_l, b_y
 
 
 class ElmoSemCorLoader(SemCorDataLoader):
+    """
+    Return ELMo character ids instead of the vocab ids of the base class as input signal.
+    """
 
     def __init__(self, dataset: SemCorDataset, batch_size: int, win_size: int, shuffle: bool = False,
-                 overlap_size: int = 0, return_all_senses: bool = False, return_pos_tags: bool = False):
-        super().__init__(dataset, batch_size, win_size, shuffle, overlap_size, return_all_senses, return_pos_tags)
+                 overlap_size: int = 0, return_all_senses: bool = False):
+        super().__init__(dataset, batch_size, win_size, shuffle, overlap_size, return_all_senses)
 
     def __next__(self):
-        if self.stop_iter:
-            raise StopIteration
-        b_x = []
-        b_y = []
-        b_l = []
+        stop_iter = False
+        b_x, b_l, b_y = [], [], []
         lengths = [len(d) for d in self.dataset.docs[self.last_doc: self.last_doc + self.batch_size]]
-        end_of_docs = max(lengths) <= self.last_offset + self.win_size
+        end_of_docs = self.last_offset + self.win_size >= max(lengths)
         for i in range(self.batch_size):
-            if self.last_doc + i >= len(self.dataset.docs) - 1:
-                self.stop_iter = True
+            if self.last_doc + i >= len(self.dataset.docs):
+                stop_iter = True
                 break
-            text_span = self.dataset.docs[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
-            labels = self.dataset.first_senses[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
+            n = self.last_doc + i
+            m = slice(self.last_offset, self.last_offset + self.win_size)
+            text_span = self.dataset.docs[n][m]
+            labels = self.dataset.first_senses[n][m]
             length = len(text_span)
             # Padding
-            text_span += ['PAD'] * (self.win_size - len(text_span))
+            text_span += ['.'] * (self.win_size - len(text_span))
             labels += [0] * (self.win_size - len(labels))
             b_x.append(text_span)
             b_y.append(labels)
@@ -166,13 +177,64 @@ class ElmoSemCorLoader(SemCorDataLoader):
         if end_of_docs:
             self.last_doc += self.batch_size
             self.last_offset = 0
+            if stop_iter or self.last_doc >= len(self.dataset.docs):
+                raise StopIteration
+
         return batch_to_ids(b_x), b_l, b_y
+
+
+class ElmoLemmaPosLoader(SemCorDataLoader):
+
+    def __init__(self, dataset: SemCorDataset, batch_size: int, win_size: int, shuffle: bool = False,
+                 overlap_size: int = 0, return_all_senses: bool = False):
+        super().__init__(dataset, batch_size, win_size, shuffle, overlap_size, return_all_senses)
+
+    def __next__(self):
+        """
+        Produce one batch.
+        :return: Tuple with:
+            - elmo char ids: Tensor
+            - lemmas: List[List[str]]
+            - pos_tags: List[List[int]]
+            - lengths: List[int]
+            - labels: List[List[int]]
+        """
+        stop_iter = False
+        b_x, b_l, b_p, b_y = [], [], [], []
+        lengths = [len(d) for d in self.dataset.docs[self.last_doc: self.last_doc + self.batch_size]]
+        end_of_docs = self.last_offset + self.win_size >= max(lengths)
+        for i in range(self.batch_size):
+            if self.last_doc + i >= len(self.dataset.docs):
+                stop_iter = True
+                break
+            n = self.last_doc + i
+            m = slice(self.last_offset, self.last_offset + self.win_size)
+            text_span = self.dataset.docs[n][m]
+            labels = self.dataset.first_senses[n][m]
+            pos_tags = self.dataset.pos_tags[n][m]
+            length = len(text_span)
+            # Padding
+            text_span += ['.'] * (self.win_size - len(text_span))
+            labels += [0] * (self.win_size - len(labels))
+            b_x.append(text_span)
+            b_y.append(labels)
+            b_l.append(length)
+            b_p.append(pos_tags)
+
+        self.last_offset += self.win_size - self.overlap_size
+        if end_of_docs:
+            self.last_doc += self.batch_size
+            self.last_offset = 0
+            if stop_iter or self.last_doc >= len(self.dataset.docs):
+                raise StopIteration
+
+        return batch_to_ids(b_x), b_x, b_p, b_l, b_y
 
 
 if __name__ == '__main__':
 
-    dataloader = SemCorDataLoader(SemCorDataset(), batch_size=4, win_size=5, shuffle=False)
+    data_loader = SemCorDataLoader(SemCorDataset(), batch_size=4, win_size=5, shuffle=False)
 
-    for idx, (bx, l, by) in enumerate(dataloader):
+    for idx, (bx, l, by) in enumerate(data_loader):
         print(bx)
         break
---
+++ testing.py
@@ -0,0 +1,177 @@
+import os
+
+import torch
+from allennlp.modules.elmo import batch_to_ids
+from torch import optim
+
+from data_preprocessing import SemCorDataLoader, SemCorDataset, ElmoSemCorLoader
+from train import BaseTrainer
+from wsd import SimpleWSD
+
+
+class ZerosDataLoader(SemCorDataLoader):
+
+    def __init__(self, dataset: SemCorDataset, batch_size: int, win_size: int, shuffle: bool = False,
+                 overlap_size: int = 0, return_all_senses: bool = False, return_pos_tags: bool = False):
+        super().__init__(dataset, batch_size, win_size, shuffle, overlap_size, return_all_senses, return_pos_tags)
+
+    def __next__(self):
+        stop_iter = False
+        b_x, b_l, b_y = [], [], []
+        lengths = [len(d) for d in self.dataset.docs[self.last_doc: self.last_doc + self.batch_size]]
+        end_of_docs = self.last_offset + self.win_size >= max(lengths)
+        for i in range(self.batch_size):
+            if self.last_doc + i >= len(self.dataset.docs):
+                stop_iter = True
+                break
+            text_span = self.dataset.docs[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
+            labels = self.dataset.first_senses[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
+            length = len(text_span)
+            # Padding
+            text_span = ['-'] * self.win_size
+            labels += [0] * (self.win_size - len(labels))
+            b_x.append(text_span)
+            b_y.append(labels)
+            b_l.append(length)
+
+        self.last_offset += self.win_size - self.overlap_size
+        if end_of_docs:
+            self.last_doc += self.batch_size
+            self.last_offset = 0
+            if stop_iter or self.last_doc >= len(self.dataset.docs):
+                raise StopIteration
+
+        return batch_to_ids(b_x), b_l, b_y
+
+
+class OneBatchDataLoader(SemCorDataLoader):
+
+    def __init__(self, dataset: SemCorDataset, batch_size: int, win_size: int, shuffle: bool = False,
+                 overlap_size: int = 0, return_all_senses: bool = False, return_pos_tags: bool = False):
+        super().__init__(dataset, batch_size, win_size, shuffle, overlap_size, return_all_senses, return_pos_tags)
+        self.one_batch = None
+
+    def __next__(self):
+        stop_iter = False
+        b_x, b_l, b_y = [], [], []
+        lengths = [len(d) for d in self.dataset.docs[self.last_doc: self.last_doc + self.batch_size]]
+        end_of_docs = self.last_offset + self.win_size >= max(lengths)
+        for i in range(self.batch_size):
+            if self.last_doc + i >= len(self.dataset.docs):
+                stop_iter = True
+                break
+            text_span = self.dataset.docs[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
+            labels = self.dataset.first_senses[self.last_doc + i][self.last_offset: self.last_offset + self.win_size]
+            length = len(text_span)
+            # Padding
+            text_span += ['PAD'] * (self.win_size - len(text_span))
+            labels += [0] * (self.win_size - len(labels))
+            b_x.append(text_span)
+            b_y.append(labels)
+            b_l.append(length)
+
+        self.last_offset += self.win_size - self.overlap_size
+        if end_of_docs:
+            self.last_doc += self.batch_size
+            self.last_offset = 0
+            if stop_iter or self.last_doc >= len(self.dataset.docs):
+                raise StopIteration
+        if self.one_batch is None:
+            self.one_batch = (batch_to_ids(b_x), b_l, b_y)
+        return self.one_batch
+
+
+class ZerosBaseTrainer(BaseTrainer):
+
+    def __init__(self,
+                 learning_rate=0.001,
+                 num_epochs=8,
+                 batch_size=32,
+                 checkpoint_path='saved_weights/zeros/checkpoint.pt'):
+        super().__init__()
+
+        self.learning_rate = learning_rate
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.checkpoint_path = checkpoint_path
+
+        # Using single GPU
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # Load data
+        dataset = SemCorDataset()
+        self.data_loader = ZerosDataLoader(dataset, batch_size=batch_size, win_size=32)
+        eval_dataset = SemCorDataset(data_path='res/wsd-test/se07/se07.xml',
+                                     tags_path='res/wsd-test/se07/se07.txt')
+        self.eval_loader = ElmoSemCorLoader(eval_dataset, batch_size=batch_size,
+                                            win_size=32, overlap_size=8)
+        # Build model
+        self.model = SimpleWSD(self.data_loader)
+        self.model.to(self.device)
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        self.best_f1_micro = 0.0
+
+        if os.path.exists(checkpoint_path):
+            checkpoint = torch.load(checkpoint_path)
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            last_epoch = checkpoint['epoch']
+            self.min_loss = checkpoint['min_loss']
+            print(f"Loaded checkpoint from: {checkpoint_path}")
+            if last_epoch >= num_epochs:
+                print("Training finished for this checkpoint")
+        else:
+            self.last_epoch = 0
+            self.min_loss = 1e3
+
+    def evaluate(self,
+                 num_epoch,
+                 eval_report='logs/zeros_report.txt',
+                 best_model_path='saved_weights/zeros/best_checkpoint.pt'):
+        super().evaluate(num_epoch, eval_report, best_model_path)
+
+
+class OneBatchBaseTrainer(BaseTrainer):
+
+    def __init__(self,
+                 learning_rate=0.001,
+                 num_epochs=8,
+                 batch_size=32,
+                 checkpoint_path='saved_weights/one_batch/checkpoint.pt'):
+        super().__init__()
+
+        self.learning_rate = learning_rate
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.checkpoint_path = checkpoint_path
+
+        # Using single GPU
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # Load data
+        dataset = SemCorDataset()
+        self.data_loader = OneBatchDataLoader(dataset, batch_size=batch_size, win_size=32)
+        eval_dataset = SemCorDataset(data_path='res/wsd-test/se07/se07.xml',
+                                     tags_path='res/wsd-test/se07/se07.txt')
+        self.eval_loader = ElmoSemCorLoader(eval_dataset, batch_size=batch_size,
+                                            win_size=32, overlap_size=8)
+        # Build model
+        self.model = SimpleWSD(self.data_loader)
+        self.model.to(self.device)
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        self.best_f1_micro = 0.0
+
+        if os.path.exists(checkpoint_path):
+            checkpoint = torch.load(checkpoint_path)
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            last_epoch = checkpoint['epoch']
+            self.min_loss = checkpoint['min_loss']
+            print(f"Loaded checkpoint from: {checkpoint_path}")
+            if last_epoch >= num_epochs:
+                print("Training finished for this checkpoint")
+        else:
+            self.last_epoch = 0
+            self.min_loss = 1e3
