fa8e787a087d198fb6463fdab6a54a24045da875
spallas <davide.spallaccini@gmail.com>
2019-11-04 14:44:01 +0100

    Add get data script.

---
+++ get_data.sh
@@ -0,0 +1,13 @@
+
+mkdir res/ logs/ saved_weights/
+mkdir res/fasttext-vectors/
+
+wget https://dl.fbaipublicfiles.com/fairseq/models/roberta.large.tar.gz
+mv roberta.large.tar.gz res/
+tar -xzf res/roberta.large.tar.gz -C res/
+
+pip install awscli
+
+aws s3 sync s3://spallas-wsd-us/wsd-test res/wsd-test/
+aws s3 sync s3://spallas-wsd-us/wsd-train res/wsd-train/
+aws s3 sync s3://spallas-wsd-us/dictionaries res/dictionaries/
