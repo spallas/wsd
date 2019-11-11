3142f7694765485f39aff23c34699bc48dd6056e
spallas <davide.spallaccini@gmail.com>
2019-11-11 12:41:13 +0100

    Implement synset lemmas vocabs building. New config.

--- build_dictionaries.py
+++ build_dictionaries.py
@@ -1,36 +1,109 @@
 import argparse
+from collections import OrderedDict
+from nltk.corpus import wordnet as wn
+from nltk.corpus.reader import Synset
+
+
+def _get_dicts(syn_lemma_vocab='res/dictionaries/syn_lemma_vocab.txt',
+               senses_vocab='res/dictionaries/senses.txt'):
+    with open(senses_vocab) as f:
+        sense2id = {line.strip().split()[0]: int(line.strip().split()[1]) for line in f}
+    wn_lemmas = OrderedDict()
+    with open(syn_lemma_vocab) as f:
+        for i, line in enumerate(f):
+            wn_lemmas[line.strip()] = i
+    return sense2id, wn_lemmas
+
+
+class SynsetMap:
+
+    def __init__(self, map_model: str, src_file: str):
+        assert map_model in ['blc', 'macro'], 'Choose between blc|macro'
+        if map_model == 'blc':
+            self._build_blc_map(src_file)
+        elif map_model == 'macro':
+            self._build_macro_map(src_file)
+        self.map_dict = OrderedDict()
+
+    def _build_blc_map(self, src_file):
+        pass
+
+    def _build_macro_map(self, src_file):
+        pass
+
+    def map_synset(self, synset: str) -> Synset:
+        return None
 
 
 def build_1hop(dest_path='res/dictionaries/sense_lemmas_1hop.txt',
                syn_lemma_vocab='res/dictionaries/syn_lemma_vocab.txt',
                senses_vocab='res/dictionaries/senses.txt'):
-    pass
+    sense2id, wn_lemmas = _get_dicts(syn_lemma_vocab, senses_vocab)
+    with open(dest_path, 'w') as f:
+        for S in sorted(sense2id.keys()):
+            synset = wn.synset(S)
+            hyper_list = synset.hypernyms()
+            hypo_list = synset.hyponyms()
+            synset_lemmas = synset.lemma_names()
+            for h_s in hyper_list + hypo_list:
+                synset_lemmas += h_s.lemma_names()
+            syn_lemma_ids = [wn_lemmas[l] for l in synset_lemmas if l in wn_lemmas]
+            print(f"{sense2id[S]}\t{syn_lemma_ids}", file=f)
 
 
 def build_blc(dest_path='res/dictionaries/sense_lemmas_blc.txt',
               syn_lemma_vocab='res/dictionaries/syn_lemma_vocab.txt',
               senses_vocab='res/dictionaries/senses.txt'):
-    pass
+    sense2id, wn_lemmas = _get_dicts(syn_lemma_vocab, senses_vocab)
+    smap = SynsetMap('blc', 'res/blc/???')
+    with open(dest_path, 'w') as f:
+        for S in sorted(sense2id.keys()):
+            actual_synset = smap.map_synset(S)
+            syn_lemma_ids = [wn_lemmas[l] for l in actual_synset.lemma_names() if l in wn_lemmas]
+            print(f"{sense2id[S]}\t{syn_lemma_ids}", file=f)
 
 
-def build_fs(dest_path='res/dictionaries/sense_lemmas_macro.txt',
-             syn_lemma_vocab='res/dictionaries/syn_lemma_vocab.txt',
-             senses_vocab='res/dictionaries/senses.txt'):
-    pass
+def build_macro(dest_path='res/dictionaries/sense_lemmas_macro.txt',
+                syn_lemma_vocab='res/dictionaries/syn_lemma_vocab.txt',
+                senses_vocab='res/dictionaries/senses.txt'):
+    sense2id, wn_lemmas = _get_dicts(syn_lemma_vocab, senses_vocab)
+    smap = SynsetMap('macro', 'res/macrosenses/???')
+    with open(dest_path, 'w') as f:
+        for S in sorted(sense2id.keys()):
+            actual_synset = smap.map_synset(S)
+            syn_lemma_ids = [wn_lemmas[l] for l in actual_synset.lemma_names() if l in wn_lemmas]
+            print(f"{sense2id[S]}\t{syn_lemma_ids}", file=f)
 
 
 def build_hyper(dest_path='res/dictionaries/sense_lemmas_hyper.txt',
-                ):
-    pass
+                syn_lemma_vocab='res/dictionaries/syn_lemma_vocab.txt',
+                senses_vocab='res/dictionaries/senses.txt'):
+    sense2id, wn_lemmas = _get_dicts(syn_lemma_vocab, senses_vocab)
+    with open(dest_path, 'w') as f:
+        for S in sorted(sense2id.keys()):
+            synset = wn.synset(S)
+            hyper_list = synset.hypernyms()
+            synset_lemmas = synset.lemma_names()
+            for h_s in hyper_list:
+                synset_lemmas += h_s.lemma_names()
+            syn_lemma_ids = [wn_lemmas[l] for l in synset_lemmas if l in wn_lemmas]
+            print(f"{sense2id[S]}\t{syn_lemma_ids}", file=f)
 
 
-def build_synset(dest_path='res/dictionaries/sense_lemmas_1hop.txt'):
-    pass
+def build_synset(dest_path='res/dictionaries/sense_lemmas_synset_.txt',
+                 syn_lemma_vocab='res/dictionaries/syn_lemma_vocab.txt',
+                 senses_vocab='res/dictionaries/senses.txt'):
+    sense2id, wn_lemmas = _get_dicts(syn_lemma_vocab, senses_vocab)
+    with open(dest_path, 'w') as f:
+        for S in sorted(sense2id.keys()):
+            synset_lemmas = wn.synset(S).lemma_names()
+            syn_lemma_ids = [wn_lemmas[l] for l in synset_lemmas]
+            print(f"{sense2id[S]}\t{syn_lemma_ids}", file=f)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Build different dictionaries")
-    pass
+    build_1hop()
 
 
 if __name__ == '__main__':
