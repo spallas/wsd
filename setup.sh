05748bc7e46de4033656a6ac2077cb4cc2052367
spallas <davide.spallaccini@gmail.com>
2019-12-05 12:10:58 +0100

    New setup bash scripts.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+
+mkdir res/ logs/ saved_weights/
+mkdir res/fasttext-vectors/
+mkdir res/dictionaries/
+
+wget https://dl.fbaipublicfiles.com/fairseq/models/roberta.large.tar.gz
+mv roberta.large.tar.gz res/
+tar -xzf res/roberta.large.tar.gz -C res/
+rm res/roberta.large.tar.gz
+
+pip install -r requirements.txt
+
+git clone https://github.com/NVIDIA/apex
+cd apex || exit
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+
+python -c "import nltk; nltk.download('wordnet')"
