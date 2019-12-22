ed7d50a234c3b2cb434d915423e1e9b88fa2dd1b
spallas <davide.spallaccini@gmail.com>
2019-12-22 10:17:48 +0100

    fix.

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ torch.manual_seed(42)
 np.random.seed(42)
 random.seed(42)
 TELEGRAM = True
-START_EVAL_EPOCH = 17
+START_EVAL_EPOCH = 14
 BATCH_MUL = CachedEmbedLoader.SINGLE
 RANDOMIZE = True
 
--- wsd.py
+++ wsd.py
@@ -155,7 +155,7 @@ class RobertaTransformerWSD(BaseWSD):
 
     def forward(self, seq_list, lengths=None, cached_embeddings=None, tags=None):
         x = self.embedding(seq_list) if cached_embeddings is None else cached_embeddings
-        # x = self.batch_norm(x)
+        x = self.batch_norm(x)
         mask = get_transformer_mask(lengths, self.win_size, self.device)
         x, h = self.transformer(x, mask)
         if tags is None:
