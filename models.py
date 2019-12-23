22ec25696cf2666e39a0cc3a7b1d99dcebec4545
spallas <davide.spallaccini@gmail.com>
2019-12-23 16:58:59 +0100

    fix.

--- models.py
+++ models.py
@@ -306,7 +306,7 @@ class LabelSmoothingLoss(nn.Module):
         output (FloatTensor): batch_size x n_classes
         target (LongTensor): batch_size
         """
-        output = output.to(device)
+        target = target.to(device)
         model_prob = self.one_hot.repeat(target.size(0), 1).to(device)
         model_prob.scatter_(1, target.unsqueeze(1), self.confidence)
         model_prob.masked_fill_((target == self.ignore_index).unsqueeze(1), 0)
